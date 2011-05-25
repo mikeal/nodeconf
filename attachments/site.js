113eea13336b47f9920be8f0d3cd78f72ac0bf53
Mikeal Rogers <mikeal.rogers@gmail.com>
2011-05-25 13:52:32 -0700

    Many, Many, updates.

--- attachments/site.js
+++ attachments/site.js
@@ -134,6 +134,23 @@ app.index = function () {
   $('div#sps-biglogo').hide();
   $('div#main-speakers').show();
   $('div#info-section').show();
+  
+  $('span.talkhook').click(function () {
+    var id = $(this).attr('id');
+    $('div#info-section').hide();
+    $('div#main-section').show().html('')
+    request({url:'/api/'+id}, function (e, resp) {
+      var u = '';
+      if (resp.twitter && resp.twitter.url) u = resp.twitter.url;
+      if (resp.github && resp.github.user.blog) u = resp.github.user.blog;
+      if (u !== '' && u.slice(0, 'http'.length) !== 'http') u = ('http://'+u);
+      $('div#main-section').html(
+          '<h3><a href="'+u+'">'+resp.name+'</a></h3>'+
+          '<h2 class="top-talk-title">'+resp.title+'</h2>'+
+          '<div class="proposal-desc">' + resp.description + '</div>'
+        )
+    })
+  })
 };
 
 var shrinkHeader = function () {
@@ -419,13 +436,37 @@ app.showSponsor = function () {
       
       , andyet : '<p><a href="http://www.andyet.net">&yet</a> (<a href="http://www.andyet.net">www.andyet.net</a>) makes web software for human people (and have a nearly inappropriate amount of fun doing it). Their team includes established contributors to the web community in XMPP and realtime web technologies, cloud cartography, and Javascript.</p>'
       
+      , 'basho': '<a href="http://www.basho.com">Basho Technologies</a> (<a href="http://www.basho.com">www.basho.com</a>), is the creator and commercial sponsor          of <a href="http://wiki.basho.com">Riak</a>, an open source, scalable, non-relational database being used in production by companies including Mozilla, Comcast, Wikia, and Mochi Media.</p> <p>Why is Basho sponsoring JSConf and NodeConf? Because we have an up-and-coming community of JavaScript users that sustain us with hard work, positivity, and code contributions, and we are committed to making their lives easier with a scalable, rock-solid, fault-tolerant database for production applications.</p> <p>Basho was founded in January 2008 by a core group of Software Architects, Engineers and Executive Leadership from Akamai Technologies and is headquartered in Cambridge, Massachusetts.</p>'
+      , 'mozilla': '<p><a href="http://mozilla.org">Mozilla</a> (<a href="http://mozilla.org">http://mozilla.org</a>) is a global community dedicated to building free, open source products and technologies that improve the online experience for people everywhere. We work in the open with a highly disciplined, transparent and cooperative development process, under the umbrella of the non-profit Mozilla Foundation. As a wholly owned subsidiary, the Mozilla Corporation organizes the development and marketing of Mozilla products. This unique structure has enabled Mozilla to financially support and cultivate competitive, viable community innovation. For more information, visit</p>'
+      , hpwebos: '<p>At <a href="http://developer.palm.com">Palm</a>, the web is at the foundation of everything we do. As the only mobile platform built on the web, Palm webOS offers developers a thriving environment for creating, distributing, and promoting applications. From an SDK based on web standards to a developer program that leverages the web to deliver openness and choice in distribution, we are making it easier than ever before to take your web skills to mobile. Of course, this is only the beginning. We believe the web is the future of mobile development, and we’re excited to be a part of the community that is driving this amazing transformation. For more information visit <a href="http://developer.palm.com">http://developer.palm.com/</a>.</p>'
+      , facebook: '<p><a href="http://www.facebook.com/engineering">Facebook</a> is a social utility that helps over 500 million people around the world communicate more efficiently with their friends, family and coworkers. The site has been built on common open source software such as Linux, Apache, MySQL, and PHP and over the past few years, the company has developed a number of <a href="http://developers.facebook.com/opensource.php">open source infrastructure technologies</a> to support the site\'s growth including <a href="http://developers.facebook.com/hiphop-php/">HipHop for PHP</a>, <a href="http://incubator.apache.org/cassandra/">Cassandra</a>, <a href="http://wiki.apache.org/hadoop/Hive">Hive</a>, <a href="http://developers.facebook.com/scribe/">Scribe</a>, and <a href="http://incubator.apache.org/thrift/">Thrift</a>. Facebook engineers actively contribute and participate in various open source projects, developing technologies that facilitate the sharing of information through the social graph.</p>'
+      , zappos: '<p>The <a href="http://www.zappos.com">Zappos</a> Family of Companies, a subsidiary of <a href="http://www.amazon.com">Amazon.com</a>, is a leader in online apparel and footwear sales by striving to provide shoppers the best possible service and selection. We carry millions of products from over 1000 footwear and apparel brands. Established in 1999, we are currently located in Las Vegas, NV, Shepherdsville, KY, and with a brand-spanking new extension software development shop, we\'re back in our "hometown," San Francisco, CA. There are approximately 2,500 Zapponians, but continual growth makes that number ever-increasing.</p><p>We\'ve been asked by a lot of people how we\'ve grown so quickly, and the answer is actually really simple...we\'ve aligned the entire organization around one mission: to provide the best customer service possible. Internally, we call this our WOW philosophy.</p><p>And, we believe that it’s our unique, “fun and a little weird” culture that drives the WOW. In January 2011, Zappos earned #6 on the 2010 Fortune: 100 Best Companies to Work For List, and our sales exceed 1 billion dollars a year. We think we might be on to something here...</p>'
+      , voxer: '<p><a href="http://www.voxer.com">Voxer</a> is a Walkie Talkie application for smartphones. With Voxer, you can send instant voice messages to your friends. Messages are streamed live as you are talking, and your friends can join you live or listen later.  Voxer is currently available for iOS devices, and an Android version will be released later this year.</p><p>Voxer uses node.js for all of our backend systems.  Check it out and see what node sounds like.'
+      , google: '<p><a href="http://www.google.com">Google</a>\'s innovative search technologies connect millions of people around the world with information every day. Founded in 1998 by Stanford Ph.D. students Larry Page and Sergey Brin, Google today is a top web property in all major global markets. Google\'s targeted advertising program provides businesses of all sizes with measurable results, while enhancing the overall web experience for users. Google is headquartered in Silicon Valley with offices throughout the Americas, Europe and Asia. For more information, visit www.google.com.</p>'
+      , rackspace: '<p><a href="http://www.rackspace.com">Rackspace</a>(r) Hosting is the world\'s leading specialist in the hosting and cloud computing industry, and the founder of OpenStack(tm), an open source cloud platform. The San Antonio-based company provides Fanatical Support(r) to its customers, across a portfolio of IT services, including Managed Hosting and Cloud Computing. In 2010, Rackspace was recognized by Bloomberg BusinessWeek as a Top 100 Performing Technology Company and listed on the InformationWeek 500 as one of the nation\'s most innovative users of business technology. The company was also positioned in the Leaders Quadrant by Gartner Inc. in the "2010 Magic Quadrant for Cloud Infrastructure as a Service and Web Hosting." For more information, visit <a href="http://www.rackspace.com">www.rackspace.com</a>.</p>'
+      , smugmug: '<p><a href="http://www.smugmug.com">SmugMug</a> sets the standard for online photo hosting and sharing. Whether you shoot photo or video, there’s room for it all with SmugMug’s totally unlimited uploads. Best of all, SmugMug never shrinks your photos to save space. Zip up and download your originals with a click, turning your beautiful photo galleries into safe backups.  With customizable privacy options, easy order fulfillment and control over your site\'s look and feel, you get tons of tools and features for as little as $5/month. Shoot, print and share with SmugMug.</p>'
+      , bloomberg: '<p><a href="http://www.bloomberg.com">Bloomberg</a> is the world\'s most trusted source of information for businesses and professionals. Bloomberg combines innovative technology with unmatched analytic, data, news, display and distribution capabilities, to deliver critical information via the BLOOMBERG PROFESSIONAL® service and multimedia platforms. Bloomberg\'s media services cover the world with more than 2,200 news and multimedia professionals at 146 bureaus in 72 countries. The BLOOMBERG TELEVISION® 24-hour network delivers smart television to more than 240 million homes. 1worldspace™ satellite radio globally and on WBBR 1130AM in New York. The award-winning monthly BLOOMBERG MARKETS® magazine, Bloomberg BusinessWeek magazine and the BLOOMBERG.COM® financial news and information Web site provide news and insight to businesses and investors.</p>'
+      , joyent: '<p>Joyent is a global cloud computing software and service provider that offers an integrated suite of cloud software and services to service providers and enterprises, and public cloud services to some of the most innovative companies in the world, such as LinkedIn, Gilt Groupe and Kabam. For more information, visit <a href="http://www.joyent.com">http://www.joyent.com</a> and <a href="http://www.joyentcloud.com">http://www.joyentcloud.com</a>.</p>'
+      , nodejitsu: '<p>Nodejitsu is a Cloud Platform-as-a-Service and marketplace for node.js applications. Founded by early adopters of node.js, Nodejitsu is committed to furthering both the node.js community and open source software. For more information visit <a href="http://nodejitsu.com">http://nodejitsu.com</a></p>'
       }[this.params.name]
   )
   $('div#sps-biglogo')
     .show()
     .html(
       { yammer: '<img class="biglogo" src="/img/Yammer.png" />'
-      , andyet: '<img class="biglogo" src="/img/andyet.png">'  
+      , andyet: '<img class="biglogo" src="/img/andyet.png"/>'  
+      , basho: '<img class="biglogo" src="/img/Basho.png"/>'  
+      , mozilla: '<img class="biglogo" src="/img/mozilla.png"/>'  
+      , facebook: '<img class="biglogo" src="/img/Facebook.png"/>'
+      , hpwebos: '<img class="biglogo" src="/img/HPwebOS.png" />'
+      , zappos: '<img class="biglogo" src="/img/ZapposFamily.png" />'
+      , voxer: '<img class="biglogo" src="/img/Voxer.png" />'
+      , google: '<img class="biglogo" src="/img/Google.png" />'
+      , rackspace: '<img class="biglogo" src="/img/Rackspace.png" />'
+      , smugmug: '<img class="biglogo" src="/img/SmugMug.png" />'
+      , bloomberg: '<img class="biglogo" src="/img/bloomberg.png" />'
+      , joyent: '<img class="biglogo" src="/img/joyent.png" />'
+      , nodejitsu: '<img class="biglogo" src="/img/nodejitsu.png" />'
       }[this.params.name]
     )
 }
