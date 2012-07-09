bbd82d26f90bb2f6a3f28af3e4f3ff182a252dfc
Mikeal Rogers <mikeal.rogers@gmail.com>
2012-07-08 19:44:05 -0700

    Adding summercamp site.

--- forwards/index.js
+++ forwards/index.js
@@ -1,6 +1,6 @@
 require('http').createServer(function (req, resp) {
   resp.statusCode = 301
-  resp.setHeader('location', 'http://www.nodeconf.com'+req.url)
+  resp.setHeader('location', 'http://www.'+req.headers.host+req.url)
   resp.end()
 }).listen(80)
 
